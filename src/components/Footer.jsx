aefbdd217ad732140aa9796917ad7e1eb3338419
codebuddyco <codebuddy.work@gmail.com>
2026-09-16 22:19:02 +0200

    bug fix 2.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,7 @@ export default function Footer(){
                     <div>
                         <Logo />
                         <p className="footer-pitch">
-                            Niezależny warsztat samochodowy w Radomiu. Regeneracja
-                            przekładni kierowniczych, serwis i naprawa mechaniki pojazdowej od 2025 roku.
+                            Regeneracja przekładni kierowniczych, serwis <br /> i naprawa mechaniki pojazdowej od 2025 roku.
                         </p>
                         <div className="footer-actions" style={{display:"flex", gap:8}}>
                             <a href="#kontakt" className="btn btn-ghost-dark btn-sm">Skontaktuj się z nami</a>
